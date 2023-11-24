6fc2b8b6b2565e82c526e5779d9c9bef4210d4d7
goldengrape <github@goldengrape.org>
2023-11-23 21:33:38 -0800

    Add sorting of x_data and sag_data

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from parse_JFL import parse_jfl_file,plot_jfl_segments_with_arrows
 from fit import fit_sag_with_multi_standard_surface,sag_of_standard_surface, show_fit_result
 import matplotlib.pyplot as plt
+import numpy as np
 
 st.markdown("# JFL to Conic Arc")
 jfl_file=st.file_uploader("Upload JFL file",type=['jfl'])
@@ -27,6 +28,11 @@ if jfl_file is not None:
     if segment != "All":
         x_data=jfl_segments[segment][:,0]
         sag_data=jfl_segments[segment][:,1]
+        # 按x_data的升序，排序x_data和sag_data
+        sort_index=np.argsort(x_data)
+        x_data=x_data[sort_index]
+        sag_data=sag_data[sort_index]
+
 
         N=st.number_input("Number of segments",value=5)
         x_data_min=st.number_input("x min",value=x_data.min())
@@ -40,7 +46,8 @@ if jfl_file is not None:
         for i, r,conic,z0,x in zip(range(N),R_list,conic_list,z0_list,part_x):
             x_min=x.min()
             x_max=x.max()
-            st.write(f"arc {i+1}: \tR={r:.4f},\tconic={conic:.4f},\tz0={z0:.4f},\tx: {x_min:.4f}→{x_max:.4f}")
+            ecc=-np.sign(conic)*np.sqrt(np.abs(conic))
+            st.write(f"arc {i+1}: \tR=\t{r:.4f},\tecc=\t{ecc:.4f}\tz0=\t{z0:.4f},\tx:\t{x_min:.4f}\t→\t{x_max:.4f},\tconic=\t{conic:.4f}")
 
         fit_plot=show_fit_result(R_list,conic_list,z0_list,part_x,part_sag,N)
         st.pyplot(fit_plot)
